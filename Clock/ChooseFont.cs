d1bf05a9ee3bce06848100da33f53081c0d0a485
hawri <hawri@DESKTOP-BMM1386>
2024-12-26 16:59:07 +0300

    Revert "Choose font Location DONE"
    This reverts commit 493d24a69159223f3c2f84116aea26eeb1515c03.

--- Clock/ChooseFont.cs
+++ Clock/ChooseFont.cs
@@ -23,15 +23,9 @@ namespace Clock
             LoadFonts();
             cbFonts.SelectedIndex = 0;
         }
-        public ChooseFontForm(MainForm parent,string font_name,int font_size)
+        public ChooseFontForm(string font_name,int font_size)
         {
             InitializeComponent();
-            this.Location = new Point
-            (
-                Screen.PrimaryScreen.Bounds.Width - parent.Width - this.Width,
-                parent.Location.Y*2
-            );
-
             Filename = font_name;
             nudFontSize.Value = font_size;
             LoadFonts();
