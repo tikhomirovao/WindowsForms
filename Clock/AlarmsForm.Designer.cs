c7b0c8bc1d7011a390fb3a1a66d0769e3ff3de11
hawri <hawri@DESKTOP-BMM1386>
2025-01-16 11:26:58 +0300

    Delete Alarm DONE

--- Clock/AlarmsForm.Designer.cs
+++ Clock/AlarmsForm.Designer.cs
@@ -43,6 +43,7 @@
             this.lbAlarms.Size = new System.Drawing.Size(379, 277);
             this.lbAlarms.TabIndex = 0;
             this.lbAlarms.DoubleClick += new System.EventHandler(this.lbAlarms_DoubleClick);
+            this.lbAlarms.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lbAlarms_KeyDown);
             // 
             // btnOK
             // 
@@ -72,6 +73,7 @@
             this.btnDelete.TabIndex = 3;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // AlarmsForm
             // 
--- Clock/AlarmsForm.cs
+++ Clock/AlarmsForm.cs
@@ -23,14 +23,14 @@ namespace Clock
         {
             InitializeComponent();
             addAlarm = new AddAlarmForm();
-            openFile= new OpenFileDialog();
+            openFile = new OpenFileDialog();
         }
         private void btnAdd_Click(object sender, EventArgs e)
         {
             addAlarm.StartPosition = FormStartPosition.Manual;
-            addAlarm.Location = new Point(this.Location.X + 25,this.Location.Y + 25);
+            addAlarm.Location = new Point(this.Location.X + 25, this.Location.Y + 25);
             DialogResult result = addAlarm.ShowDialog();
-            if (result == DialogResult.OK) 
+            if (result == DialogResult.OK)
             {
                 lbAlarms.Items.Add(new Alarm(addAlarm.Alarm));
             }
@@ -38,12 +38,31 @@ namespace Clock
 
         private void lbAlarms_DoubleClick(object sender, EventArgs e)
         {
-            addAlarm.Alarm = lbAlarms.SelectedItem as Alarm;
-            if(addAlarm.ShowDialog() == DialogResult.OK) 
+            if (lbAlarms.Items.Count > 0)
             {
-                //lbAlarms.SelectedItem = new Alarm(addAlarm.Alarm);
-                lbAlarms.Items[lbAlarms.SelectedIndex] = addAlarm.Alarm;
+                if (lbAlarms.SelectedItem != null)
+                {
+                    addAlarm.Alarm = lbAlarms.SelectedItem as Alarm;
+                    addAlarm.Location = new Point(this.Location.X + 25, this.Location.Y + 25);
+                    if (addAlarm.ShowDialog() == DialogResult.OK)
+                    {
+                        //lbAlarms.SelectedItem = new Alarm(addAlarm.Alarm);
+                        lbAlarms.Items[lbAlarms.SelectedIndex] = addAlarm.Alarm;
+                    }
+                }
+                else MessageBox.Show(this, "Выберите будильник", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+            else btnAdd_Click(sender, e);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            lbAlarms.Items.Remove(lbAlarms.SelectedItem);
+        }
+
+        private void lbAlarms_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete) btnDelete_Click(sender, e);
         }
     }
 }
